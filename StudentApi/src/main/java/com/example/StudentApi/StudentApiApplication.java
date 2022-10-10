8ca35fcdd80c24eda5e39a4504727e711c78aea4
uk47kumar <uk47kumar@gmail.com>
2022-10-11 01:45:07 +0530

    Student API first commit

---
+++ StudentApi/src/main/java/com/example/StudentApi/StudentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApiApplication.class, args);
+	}
+
+}
